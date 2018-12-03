96751260a2c7e4d8066377370987273f9c8b65e3
Dahmitri Stephenson <dstephenon@blim.com>
2018-12-03 13:36:10 -0500

    Exported checkpointer data and disabled internal checkpointing.

--- shard_consumer.go
+++ shard_consumer.go
@@ -156,15 +156,17 @@ mainloop:
 		select {
 		case <-k.stop:
 			return
-		case <-commitTicker:
-			finishCommitted, err := checkpointer.Commit()
-			if err != nil {
-				k.shardErrors <- shardConsumerError{shardID: shardID, action: "checkpointer.Commit", err: err}
-				return
-			}
-			if finishCommitted {
-				return
-			}
+			/*
+				case <-commitTicker:
+					finishCommitted, err := checkpointer.Commit()
+					if err != nil {
+						k.shardErrors <- shardConsumerError{shardID: shardID, action: "checkpointer.Commit", err: err}
+						return
+					}
+					if finishCommitted {
+						return
+					}
+			*/
 			// Go back to waiting for a throttle/stop.
 			continue mainloop
 		case <-nextThrottle:
@@ -206,19 +208,21 @@ mainloop:
 				// Loop until we stop or the record is consumed, checkpointing if necessary.
 				for {
 					select {
-					case <-commitTicker:
-						finishCommitted, err := checkpointer.Commit()
-						if err != nil {
-							k.shardErrors <- shardConsumerError{shardID: shardID, action: "checkpointer.Commit", err: err}
-							return
-						}
-						if finishCommitted {
-							return
-						}
+					/*
+						case <-commitTicker:
+							finishCommitted, err := checkpointer.Commit()
+							if err != nil {
+								k.shardErrors <- shardConsumerError{shardID: shardID, action: "checkpointer.Commit", err: err}
+								return
+							}
+							if finishCommitted {
+								return
+							}
+					*/
 					case <-k.stop:
 						return
-					case k.records <- &consumedRecord{
-						record:       record,
+					case k.records <- &ConsumedRecord{
+						Record:       record,
 						Checkpointer: checkpointer,
 						retrievedAt:  retrievedAt,
 					}:
